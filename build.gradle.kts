1761f84bd1a110c60efdc4479e4d16ad7f77e545
alevtina.shalygina <alevtina.shalygina@team.wrike.com>
2026-02-09 13:01:26 +0100

    tests cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    id("io.qameta.allure-report") version "3.0.1"
 }
 
 group = "org.example"
@@ -11,13 +12,26 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("io.qameta.allure:allure-junit5")
+    testImplementation(platform("io.qameta.allure:allure-bom:2.25.0"))
     implementation("io.rest-assured:rest-assured:6.0.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(24)
+    outputs.upToDateWhen { false }
+    ignoreFailures = true
+
+    environment("JB_API_KEY", System.getenv("JB_API_KEY"))
+    environment("JB_API_KEY_ORG_VIEWER", System.getenv("JB_API_KEY_ORG_VIEWER"))
+    environment("JB_API_KEY_TEAM_AB_ADMIN", System.getenv("JB_API_KEY_TEAM_AB_ADMIN"))
+    environment("JB_API_KEY_TEAM_B_ADMIN", System.getenv("JB_API_KEY_TEAM_B_ADMIN"))
+    environment("JB_API_KEY_TEAM_B_VIEWER", System.getenv("JB_API_KEY_TEAM_B_VIEWER"))
+    environment("JB_CUSTOMER_CODE", System.getenv("JB_CUSTOMER_CODE"))
+
 }
